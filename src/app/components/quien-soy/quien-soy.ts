2c41a6a83cb93838fa479147845934fa820e19a6
eliascharadia <eliascharadia01@gmail.com>
2026-05-15 20:24:54 -0300

    add: funcionalidad, estilos del componente quien soy

--- src/app/components/quien-soy/quien-soy.ts
+++ src/app/components/quien-soy/quien-soy.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { QuienSoyService } from '../../services/quien-soy';
 
 @Component({
   selector: 'app-quien-soy',
@@ -7,4 +8,13 @@ import { RouterModule } from '@angular/router';
   templateUrl: './quien-soy.html',
   styleUrl: './quien-soy.css',
 })
-export class QuienSoy {}
+export class QuienSoy implements OnInit{
+
+  private quienSoyService = inject(QuienSoyService);
+
+  user = this.quienSoyService.userA;
+
+  ngOnInit(): void {
+    this.quienSoyService.loadUser();
+  }
+}
