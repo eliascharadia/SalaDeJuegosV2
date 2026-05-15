ed25d8c1f8c9cdabd7e2655158f92df8e6cd5cd8
eliascharadia <eliascharadia01@gmail.com>
2026-05-15 19:42:35 -0300

    add: hice logica para que los componentes puedan navegar entre si

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
+import { QuienSoy } from './components/quien-soy/quien-soy';
+import { BienvenidaHome } from './components/bienvenida-home/bienvenida-home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: BienvenidaHome },
+    { path: 'login', component: Login },
+    { path: 'register', component: Registro },
+    { path: 'quiensoy', component: QuienSoy },
+    { path: '**', redirectTo: 'home' }
+];
--- src/app/components/quien-soy/quien-soy.ts
+++ src/app/components/quien-soy/quien-soy.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-quien-soy',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './quien-soy.html',
   styleUrl: './quien-soy.css',
 })
