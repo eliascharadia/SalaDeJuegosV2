97a3c2b3019d58295f383f9417fbc0de4af350c5
eliascharadia <eliascharadia01@gmail.com>
2026-05-15 19:20:52 -0300

    creacion del componente bienvenidaHome

---
+++ src/app/components/bienvenida-home/bienvenida-home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenida-home',
+  imports: [],
+  templateUrl: './bienvenida-home.html',
+  styleUrl: './bienvenida-home.css',
+})
+export class BienvenidaHome {}
