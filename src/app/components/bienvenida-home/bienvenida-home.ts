de894c2b37a873b411573af7f3472653cee2c0bf
eliascharadia <eliascharadia01@gmail.com>
2026-05-15 21:43:26 -0300

    add: modales al componente bienvenida

--- src/app/components/bienvenida-home/bienvenida-home.ts
+++ src/app/components/bienvenida-home/bienvenida-home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-bienvenida-home',
@@ -7,4 +8,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './bienvenida-home.html',
   styleUrl: './bienvenida-home.css',
 })
-export class BienvenidaHome {}
+export class BienvenidaHome {
+  private auth = inject(Auth);
+  private router = inject(Router)
+
+  estaLogeado = this.auth.isAutheticated();
+  showLoginModal = signal(false);
+
+  goToGame(route: string) {
+
+    if (this.estaLogeado) {
+
+      this.router.navigate([route]);
+    } else {
+
+      this.showLoginModal.set(true);
+    }
+  }
+}
