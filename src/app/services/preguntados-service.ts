25b11143ea140f2a32360431c3a61e5dd9bd5824
eliascharadia <eliascharadia01@gmail.com>
2026-05-26 00:04:25 -0300

    add: servicios para usar en el juego preguntados

---
+++ src/app/services/preguntados-service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Pregunta } from '../models/pregunta';
+import { Auth } from './auth';
+import { SupabaseService } from './supabase';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PreguntadosService {
+  private http = inject(HttpClient);
+  private apiUrl = "https://opentdb.com/api.php?amount=20&category=11&difficulty=easy&type=multiple";
+  private auth = inject(Auth);
+  private supabase = inject(SupabaseService);
+
+  resultados = signal<Pregunta[]>([])
+
+  error = signal('');
+  loading = signal(false);
+
+  obtenerPreguntas(): void {
+    this.loading.set(true);
+
+    this.http.get<any>(this.apiUrl).subscribe({
+        next: (data) => {
+          const resultados = data.results;
+          this.resultados.set(resultados);
+          this.loading.set(false);
+      },
+      error: (_err) => {
+        console.log(_err);
+        this.error.set('Error al obtener preguntas');
+        this.loading.set(false);
+      }
+    })
+  }
+
+  async guardarPartida(puntaje: number) {
+
+    const user = this.auth.user();
+
+    if (!user) return;
+
+    const { error } = await this.supabase.getClient()
+      .from('partidas_preguntados')
+      .insert({
+        usuario_id: user.id,
+        puntaje: puntaje
+      });
+
+      if (error) {
+      console.error('Error guardando partida:', error.message);
+      return false;
+    }
+
+    return true;
+  }
+
+}
